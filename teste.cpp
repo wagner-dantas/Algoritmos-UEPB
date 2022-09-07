044c31e9a2506fcdff66b023e01e14e7b95fba0c
Wagner Dantas Martins <wagner-hora@hotmail.com>
2022-09-06 21:29:10 -0300

    teste em C++
    iniciando na linguagem de programação em C++

---
+++ teste.cpp
@@ -0,0 +1,13 @@
+/* aqui é o comentário de varias linhas
+######
+*/
+// comentário de uma linha 
+#include <iostream>
+
+ using namespace std;
+
+ int main()
+ {
+ cout << "Hello world! My name is Wagner" << endl;
+ return 0;
+ }
